535b785ec381c03fe78833916beef894e07e39b4
carpioe <56162609+carpioe@users.noreply.github.com>
2019-11-29 19:42:47 -0500

    Update textbookprogrammingassignment.py

--- textbookprogrammingassignment.py
+++ textbookprogrammingassignment.py
@@ -1,1 +1,13 @@
 #Eddie Carpio
+#11/19/2019
+#project
+class person():
+
+    def __init__(self,first,last,age):
+        self.first = first
+        self.last = last
+        self.age = age
+
+    def description(self):
+        author = (f'{self.first+self.last+self.age}')
+        return author
